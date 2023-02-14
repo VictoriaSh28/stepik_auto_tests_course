5dde013733046cb730c27dad8af4dfe13a5a4367
VictoriaSh28 <102437060+VictoriaSh28@users.noreply.github.com>
2023-02-14 19:53:10 +0200

    Update 1-6-10.py

--- 1-6-10.py
+++ 1-6-10.py
@@ -38,10 +38,3 @@ finally:
 
 
 
-'https://stepik.org/lesson/236895/step/1', 'https://stepik.org/lesson/236896/step/1',
-                            'https://stepik.org/lesson/236897/step/1',
-                            'https://stepik.org/lesson/236898/step/1',
-                            'https://stepik.org/lesson/236899/step/1',
-                            'https://stepik.org/lesson/236903/step/1',
-                            'https://stepik.org/lesson/236904/step/1',
-                            'https://stepik.org/lesson/236905/step/1'
